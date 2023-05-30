b7802be203682d5210732ca083dce3beac6ce7f8
Kevin Blanchard <blanchardkevinj@gmail.com>
2023-05-30 06:57:00 -0400

    Fix bugs

--- GoonBowlingTracker/ContentView.swift
+++ GoonBowlingTracker/ContentView.swift
@@ -15,19 +15,35 @@ class NumbersOnly: ObservableObject {
             let filtered = value.filter { $0.isNumber && Int(value) != 0 }
             
             if value != filtered {
-                value = filtered
+                let valueInt = Int(value) ?? -1
+                if valueInt < 0 {
+                    value = "0"
+                }
+               else if 0...300 ~= valueInt {
+                    value = filtered
+                }
+                else {
+                   value = "300"
+                }
             }
         }
     }
+    func IsValidScore() -> Bool {
+        return 0...300 ~= Int(value) ?? -1
+    }
+        
     func GetBowlingScore() -> Int16 {
         let score = Int16(value) ?? 0
         return 0...300 ~= score ? score : 0
     }
+    
 }
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @State var Score: NumbersOnly = NumbersOnly()
+    @State var Average: Int = 0
+    @FocusState private var scoreIsFocused: Bool
 
 
     @FetchRequest(
@@ -37,30 +53,49 @@ struct ContentView: View {
 
     var body: some View {
         
-        VStack {
-            HStack {
-                Text("Total Average: ")
-                Text(String(calculateAverage()))
-            }
-
-            HStack {
-                Text("Add Score")
-                TextField("Score", text: $Score.value)
-                    .keyboardType(.decimalPad)
-            }
-            Divider()
-
             NavigationView {
                 List {
-                    ForEach(scores) { item in
-                        NavigationLink {
-                            Text("Score: \(item.score) on \n \(item.time!, formatter: itemFormatter)")
-                        } label: {
-//                            Text(item.time!, formatter: itemFormatter)
-                            Text(String(item.score))
-                        }
+                    HStack {
+                        Text("Total Average: ")
+                        Text(String(Average))
                     }
-                    .onDelete(perform: deleteItems)
+
+                    HStack {
+                        Text("Add Score")
+                        TextField("Score", text: $Score.value)
+                            .keyboardType(.decimalPad)
+                            .focused($scoreIsFocused)
+                    }
+                    Section {
+                        ForEach(LastFiveRange(), id:\.self) { index in
+                            let item = scores[index]
+                            NavigationLink {
+                                Text("Score: \(item.score) on \n \(item.time!, formatter: itemFormatter)")
+                                .navigationTitle("Score Details")
+                            } label: {
+                                Text(String(item.score))
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                            
+                    } header: {
+                       Text("Recent Games")
+                    }
+                    Section {
+                        ForEach(scores) { item in
+                            
+                            NavigationLink {
+                                Text("Score: \(item.score) on \n \(item.time!, formatter: itemFormatter)")
+                                .navigationTitle("Score Details")
+                            } label: {
+                                Text(String(item.score))
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                        
+                    } header: {
+                        Text("History")
+                    }
                 }
                 .toolbar {
     #if os(iOS)
@@ -73,25 +108,47 @@ struct ContentView: View {
                             Label("Add Score", systemImage: "plus")
                         }
                     }
+                    ToolbarItem {
+                        Button(action: refresh) {
+                            Label("Refresh", systemImage: "arrow.clockwise")
+                        }
+                    }
                 }
+                .navigationTitle("Home")
                 Text("Select an item")
-            }
+            }.onAppear(perform: calculateAverage)
             
-        }
     }
     
-    private func calculateAverage() -> Int {
+    private func calculateAverage() {
         var total = 0
         var count = 0
         for score in scores {
             total += Int(score.score)
             count += 1
         }
-        return count <= 0 ? 0 : total / count
+//        Average =
+        Average = count <= 0 ? 0 : total / count
+        
+    }
+    
+    private func LastFiveRange() -> Range<Int> {
+        let count = scores.count
+        return (0..<5 ~= count) ? 0..<count : 0..<5
+      
+    }
+    
+    private func refresh() {
+        calculateAverage()
+        scoreIsFocused = false
         
     }
 
     private func addItem() {
+        scoreIsFocused = false
+        guard Score.IsValidScore() else {
+            return
+        }
         withAnimation {
             let newScore = BowlingScore(context: viewContext)
             newScore.score = Score.GetBowlingScore()
@@ -99,6 +156,7 @@ struct ContentView: View {
 
             do {
                 try viewContext.save()
+                calculateAverage()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -114,6 +172,7 @@ struct ContentView: View {
 
             do {
                 try viewContext.save()
+                calculateAverage()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
